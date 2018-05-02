017d3f16ac084f375638edfbeacd065868b3bb18
luwis lopez <spartanhero93@gmail.com>
2018-05-02 16:16:38 -0400

    http module

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const os = require('os')
 const fs = require('fs')
 const log = require('./logger')
-const EventEmitter = require('events')
+const http = require('http')
 
 function checkMemory () {
   const [totalMemory, freeMemory] = [
@@ -12,9 +12,30 @@ function checkMemory () {
 }
 
 function readFilesNames () {
-  const [files, folders] = [fs.readdirSync('./'), fs.readdir]
+  const files = fs.readdirSync('./')
   log(files)
+}
+
+function createServer () {
+  const server = http.createServer((req, res) => {
+    switch (req.url) {
+      case '/':
+        res.write('Hello Best Waifus')
+        res.end()
+        break
+      case '/waifus':
+        res.write(JSON.stringify(['Isla', 'Taiga', 'Yuno']))
+        res.end()
+      default:
+        break
+    }
+  })
+
+  server.listen(9000)
+
+  console.log('listening on port 9000')
 }
 
 checkMemory()
 readFilesNames()
+createServer()
